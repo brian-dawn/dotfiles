18247a0fa1e55a93a7d3555f4beead0c6b84b69b
Brian Dawn <brian.t.dawn@gmail.com>
2016-07-08 16:42:12 -0500

    Syncing

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,6 @@
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/Users/brian/.config/omf"
-
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
